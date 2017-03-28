186f24d069629b820079c495f6f466faea1c5f28
emartin <emartin>
2017-03-28 19:57:47 +0200

    Ejemplo Spring Boot

--- 01_MVC/src/main/java/com/curso/spring/mvc/controller/PersonaRestController.java
+++ 01_MVC/src/main/java/com/curso/spring/mvc/controller/PersonaRestController.java
@@ -1,8 +1,16 @@
 package com.curso.spring.mvc.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,38 +19,69 @@ import org.springframework.web.bind.annotation.RestController;
 import com.curso.spring.mvc.entidades.Persona;
 
 @RestController
-@RequestMapping(path="/Persona", consumes="application/json", produces="application/json")
+@RequestMapping(path="/Persona")
 public class PersonaRestController {
 
-	@RequestMapping(path="/{name}", method=RequestMethod.GET)
-	public Persona consultar(@PathVariable("name") String nombre){
+	@RequestMapping(path="/{name}", method=RequestMethod.GET, produces="application/json")
+	//@ResponseBody //No hacefalta ponerlo porque es implicito con @Restcontroller
+	public ResponseEntity<Persona> consultar(@PathVariable("name") String nombre){
+		
+		//Invoacion de una determinada logica de negocio
+		Persona resultadoNegocio = new Persona(nombre, 22);
+		
+		//Preparacion de la respuesta
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpStatus status = HttpStatus.OK;
+		
+		ResponseEntity<Persona> respuesta = new ResponseEntity<Persona>(resultadoNegocio, headers, status);
+		return respuesta;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, produces="application/json")
+	public ResponseEntity<List<Persona>> consultarPaginado(
+			@RequestParam(name="age", required=false) int edad, 
+			@RequestParam(required=false) Integer from, 
+			@RequestParam(required=false) Integer size, 
+			@RequestParam(required=false) Integer count){
 		return null;
 	}
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Persona> consultar(){
+	@RequestMapping(path="/{name}/padre", method=RequestMethod.GET, produces="application/json")
+	public ResponseEntity<Persona> consultarPadre(@PathVariable("name") String nombre){
 		return null;
 	}
 	
-	//Persona?age=12
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Persona> consultar(@RequestParam("age") int edad){
-		return null;
+	@RequestMapping(method=RequestMethod.POST, consumes="application/json")
+	public ResponseEntity<Void> insertar(@RequestBody Persona persona) throws URISyntaxException{
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.setLocation(new URI("/Persona/" + persona.getNombre()));
+		
+		HttpStatus status = HttpStatus.CREATED;
+		
+		return new ResponseEntity<>(headers, status);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	public void insertar(Persona persona){
+	@RequestMapping(method=RequestMethod.PUT, consumes="application/json")
+	public ResponseEntity<Void> modificar(@RequestBody Persona persona) throws URISyntaxException{
+		HttpHeaders headers = new HttpHeaders();
 		
+		headers.setLocation(new URI("/Persona/" + persona.getNombre()));
+		
+		HttpStatus status = HttpStatus.CREATED;
+		
+		return new ResponseEntity<>(headers, status);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT)
-	public void modificar(Persona persona){
+	@RequestMapping(path="/{name}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> borrar(@PathVariable("name") String nombre){
+		MultiValueMap<String, String> headers = null;
+		HttpStatus status = null;
 		
-	}
-	
-	@RequestMapping(method=RequestMethod.DELETE)
-	public void borrar(String nombre){
-		
+		return new ResponseEntity<>(headers, status);
 	}
 	
 }
---
+++ 02_MVC_Boot/src/main/java/com/curso/spring/ConfiguracionWebMvc.java
@@ -0,0 +1,31 @@
+package com.curso.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//No es necesario, ya que @SpringBootApplication añade el escaneo del paquete "com.curso.spring" 
+//@ComponentScan("com.curso.spring.mvc.controller")
+//No es necesario añadirlo porque la anotacion @SpringBootApplication lo añade por el hecho de tner en el classpath el starter web
+//@EnableWebMvc
+public class ConfiguracionWebMvc {
+
+	//Si se necesitarian definir Beans de Negocio
+	@Bean
+	public String saludo(){
+		return new String("Hola Mundo!!!!");
+	}
+	
+	@Bean
+	public String despedida(){
+		return new String("Adios Mundo!!!!");
+	}
+	
+	//No es necesario porque el starter de thymeleaf añade un ViewResolver para las plantillas de thymeleaf
+	/*@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}*/
+}
